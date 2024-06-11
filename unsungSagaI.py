63148943b3d41461ea07873051c54b273dc177c5
KenniBlank <keniblank@gmail.com>
2024-06-12 03:34:12 +0545

    better

--- unsungSagaI.py
+++ unsungSagaI.py
@@ -15,7 +15,7 @@ max_height = display_info.current_h
 screen = pygame.display.set_mode((max_width, max_height))
 pygame.mouse.set_visible(False)
 
-# using os library to import all of images from specified path
+# Function to load and scale images from a specified folder
 def loadImages(folderPath, imageSize):
     image_files = [imageFile for imageFile in os.listdir(folderPath) if imageFile.lower().endswith('.png')]
     images = []
@@ -30,7 +30,7 @@ def loadImages(folderPath, imageSize):
 gunCross = pygame.image.load('images/cross.png').convert_alpha()
 gunCross = pygame.transform.scale(gunCross, (20, 20))
 
-# Load and resize background image using PIL
+# Load and resize background image
 background = pygame.image.load('images/background.jpeg').convert()
 backgroundImage = pygame.transform.scale(background, (max_width, max_height))
 
@@ -39,122 +39,112 @@ def main():
     gameloop()
 
 def start():
-    introOutro('images/intro', 0)   
+    introOutro('images/intro', 0)
 
-
-def introOutro(folderPath , number):
-    # text
+def introOutro(folderPath, number):
+    # Text configuration
     text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-    if number == 0:
-        text = "Press Enter to Play"
-    else:
-        text = "You are the Unsung hero of this city"
-    
-    text_surface = text_font.render(text,False, 'White')
+    text = "Press Enter to Play" if number == 0 else "You are the Unsung hero of this city"
+    text_surface = text_font.render(text, False, 'White')
 
     Images = loadImages(folderPath, (max_width, max_height))
     Index = 0
     while True:
-        if Index < len(Images) - 1:
-            Index += 1
-        else:
-            Index = 0
-        screen.blit(Images[Index], (0,0))
+        screen.blit(Images[Index], (0, 0))
         screen.blit(text_surface, (max_width / 2 - 7 * len(text), max_height / 4))
         pygame.display.flip()
         clock.tick(10)
+        
+        Index = (Index + 1) % len(Images)  # Loop through images
+
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE or (event.key == pygame.K_RETURN and number == 1):
+                if event.key in [pygame.K_ESCAPE, pygame.K_RETURN]:
+                    if event.key == pygame.K_RETURN and number == 0:
+                        return
                     pygame.quit()
                     exit()
-                elif event.key == pygame.K_RETURN and number == 0:
-                    return
-
             elif event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-        
-
 
 def gameloop():
-    # gun
+    # Gun configuration
     shoot = False
     currentImageIndex = 0
     gunImages = loadImages('images/handgun', (250, 250))
     
-    # bat
+    # Bat configuration
     scaleBat = 20
     batImages = loadImages('images/batHorizontal', (scaleBat, scaleBat))
-    batImages2 = loadImages('images/batVertical', (scaleBat,scaleBat))
-    x = random.randint(20, max_width - 40)
-    y = random.randint(20, max_height - 300)
-    batIndex1 = 0
-    batIndex2 = 0
+    batIndex = 0
     batShot = 0
+    x, y = random.randint(20, max_width - 40), random.randint(20, max_height - 300)
 
-    # zombie
-    scaleZombie = 10
-    zombieImages = loadImages('images/zombie', (scaleZombie, scaleZombie))
-    zombieImages2 = loadImages('images/zombie2', (scaleZombie, scaleZombie))
-    zombieIndex1 = 0
-    zombieIndex2 = 0
+    # Zombie configuration
+    scaleZombie = 30
+    zombieImages = loadImages('images/zombie2', (scaleZombie, scaleZombie))
+    zombieIndex = 0
+    zombieShot = 0
+    zombieX, zombieY = random.randint(max_width/ 4, max_width - max_width/4), max_height - 400
 
-
+    # Fonts and animations
     text_font = pygame.font.Font('font/Pixeltype.ttf', 30)
-    foranimation = {}
+    batAnimation = {}
+    zombieAnimation = {}
 
     while True:
         mouse_pos = pygame.mouse.get_pos()
-        gunCrossRect = gunCross.get_rect(center=(mouse_pos[0], mouse_pos[1]))
+        gunCrossRect = gunCross.get_rect(center=mouse_pos)
 
-        # Scaling the bat
-        tempBatImage = pygame.transform.scale(batImages[batIndex1], (scaleBat, scaleBat))
-        if int(scaleBat) == 100:
+        # Update zombie image
+        zombieImage = pygame.transform.scale(zombieImages[zombieIndex], (scaleZombie, scaleZombie))
+        zombieIndex = (zombieIndex + 1) % len(zombieImages)
+        zombieY += 2
+
+        # Update bat image
+        batImage = pygame.transform.scale(batImages[batIndex], (scaleBat, scaleBat))
+        batIndex = (batIndex + 1) % len(batImages)
+
+        if int(scaleBat) >= 100:
             lose()
-        if batIndex1 < len(batImages) - 1:
-            batIndex1 += 1
-        else:
-            batIndex1 = 0
 
-        batKilled = f"BatKilled: {batShot}"
-        text_surface = text_font.render(batKilled, False, 'White')
+        # Rendering the screen
         screen.blit(backgroundImage, (0, 0))
         screen.blit(gunCross, gunCrossRect)
-        screen.blit(tempBatImage, (x, y));
-        screen.blit(text_surface, (max_width - 15 * len(batKilled), 50))
-        for value in list(foranimation):
-            
-            screen.blit(pygame.transform.scale(pygame.transform.flip(batImages[3], False, True), (foranimation[value][1],foranimation[value][1])), (value, foranimation[value][0]))
-            if (foranimation[value][0] > (max_height * 0.75)):
-                pass
-            else:
-                foranimation[value][0] += 5
+        screen.blit(batImage, (x, y))
+        screen.blit(zombieImage, (zombieX, zombieY))
 
+        batKilledText = f"Bat Killed: {batShot}"
+        batKilledSurface = text_font.render(batKilledText, False, 'White')
+        screen.blit(batKilledSurface, (max_width - 15 * len(batKilledText), 50))
+
+        for value in list(batAnimation):
+            screen.blit(pygame.transform.scale(pygame.transform.flip(batImages[3], False, True), (batAnimation[value][1], batAnimation[value][1])), (value, batAnimation[value][0]))
+            if batAnimation[value][0] > max_height * 0.75:
+                del batAnimation[value]
+            else:
+                batAnimation[value][0] += 5
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_ESCAPE:
-                    introOutro('images/outro', 1)
-                    exit()
+            if event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
+                introOutro('images/outro', 1)
             if event.type == pygame.MOUSEBUTTONUP:
-
                 shoot = True
-                bat_rect = tempBatImage.get_rect(topleft=(x, y))
-
-                if bat_rect.collidepoint(mouse_pos):
+                if batImage.get_rect(topleft=(x, y)).collidepoint(mouse_pos):
                     batShot += 1
-                    tempX = x
-                    tempY = y
-                    foranimation[x] = [y, scaleBat];
+                    batAnimation[x] = [y, scaleBat]
                     scaleBat = 20
+                    x, y = random.randint(20, max_width - 20), random.randint(20, max_height - 300)
+                if zombieImage.get_rect(topleft=(zombieX, zombieY)).collidepoint(mouse_pos):
+                    zombieShot += 1
+                    zombieAnimation[zombieX] = [zombieY, scaleZombie]
+                    scaleZombie = 30
+                    zombieX, zombieY = random.randint(100, max_width - 100), max_height - 300
 
-                    x = random.randint(20, max_width - 20)
-                    y = random.randint(20, max_height - 300)
-        
         if shoot:
             currentImageIndex += 1
             if currentImageIndex >= len(gunImages):
@@ -162,13 +152,13 @@ def gameloop():
                 currentImageIndex = 0
 
         scaleBat += random.random()
+        scaleZombie += random.random() * 5
 
-        # magic numbers for the gun image dont change 
+        # Display gun image
         screen.blit(gunImages[currentImageIndex], (mouse_pos[0] - 110, max_height - 250))
 
         pygame.display.flip()
         clock.tick(24)
-
 
 def lose():
     pygame.quit()
