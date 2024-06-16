b1e630d02cd6d2ef6b570e27dd135dc40d0a8612
KenniBlank <keniblank@gmail.com>
2024-06-16 19:24:07 +0545

    bunch of Improvement

--- class.py
+++ class.py
@@ -9,7 +9,7 @@ class Gun(pygame.sprite.Sprite):
     gunY = 0
     kills = 0
 
-    def __init__(self, bullet, listOfImages, screen, delay = 1, filePathForCross = "images/cross.png", filePathForBullet = "images/bullet.png"):
+    def __init__(self, bullet, listOfImages, screen, delay = 50, filePathForCross = "images/cross.png", filePathForBullet = "images/bullet.png", maxBullet = 12):
         super().__init__()
         self.listOfImages = listOfImages
         self.image = listOfImages[0]
@@ -22,6 +22,7 @@ class Gun(pygame.sprite.Sprite):
         self.gunYTemp = Gun.max_height
         self.gunCrossImage = pygame.transform.scale(pygame.image.load(filePathForCross).convert_alpha(), (30, 30))
         self.bullet = pygame.transform.scale(pygame.image.load(filePathForBullet).convert_alpha(), (40, 40))
+        self.maxBullet = maxBullet
     
     def update(self, shotFired = False, bullet = 0):
         Gun.gunX = pygame.mouse.get_pos()[0]
@@ -31,7 +32,6 @@ class Gun(pygame.sprite.Sprite):
             self.shotFired = shotFired
         self.draw()
         if bullet > 0:
-            self.gunReloads()
             self.updateBulletCount(bullet)
     
     def updateBulletCount(self, numberOfBullets):
@@ -40,7 +40,10 @@ class Gun(pygame.sprite.Sprite):
     def draw(self):
         self.bulletRemaining()
         self.gunCross()
-        self.gunAnimation()        
+        if self.bulletLeft == 0: 
+            self.gunReload()
+        else:
+            self.gunAnimation()
         
     def gunAnimation(self):
         # Check and change hand for gun
@@ -56,16 +59,15 @@ class Gun(pygame.sprite.Sprite):
             self.rect = self.image.get_rect(midbottom = (Gun.gunX, self.gunYTemp))
             tmpImage = pygame.transform.flip(self.image, False, False)
         
-        # if shotfired what to do
+        # if shotfired what to do.
         if self.shotFired == True and self.bulletLeft > 0:
+            pygame.time.delay(self.delay)
             self.screen.blit(tmpImage, self.rect.topleft)
             self.gunImageIndex += 1
-            
             if self.gunImageIndex >= len(self.listOfImages):
                 self.gunImageIndex = 0
                 self.shotFired = False
                 self.bulletLeft -= 1
-
             self.image = self.listOfImages[self.gunImageIndex]
         else:
             self.screen.blit(tmpImage, self.rect.topleft)
@@ -82,7 +84,25 @@ class Gun(pygame.sprite.Sprite):
             text = f"Bullet Left: {self.bulletLeft} X "
             textSurface = font.render(text, False, 'Black')
             self.screen.blit(textSurface, (Gun.max_width - 10 * len(text) - 60, 45))
-            self.screen.blit(self.bullet, (Gun.max_width - 90, 30))      
+            self.screen.blit(self.bullet, (Gun.max_width - 90, 30)) 
+
+    def bulletShot(self, bullet):
+        if bullet > 0:
+            pygame.mixer.music.load("audio/gunShot.mp3")
+            pygame.mixer.music.set_volume(0.5)
+            pygame.mixer.music.play()
+    
+    def gunReload(self):
+        pygame.mixer.music.load("audio/gunReload.wav")
+        pygame.mixer.music.set_volume(0.7)
+        pygame.mixer.music.play()
+        pygame.display.flip()
+        self.bulletRemaining()
+        pygame.display.flip()
+        tmp = self.maxBullet - self.bulletLeft
+        self.bulletLeft = 6
+        pygame.time.wait(int(pygame.mixer.Sound("audio/gunReload.wav").get_length() * 400))
+        return True
 
 
 def main():
@@ -96,24 +116,25 @@ def main():
     screen = pygame.display.set_mode((max_width, max_height))
     pygame.mouse.set_visible(False)
 
-    gun = Gun(20, loadImages('images/handGun', (200, 250)), screen, 0.5, 'images/cross.png', 'images/bullet.png')
+    gun = Gun(20, loadImages('images/handGun', (200, 250)), screen, 25, 'images/cross.png', 'images/bullet.png', 100)
     while True:
         screen.fill((255, 255, 255))
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
             if event.type == pygame.MOUSEBUTTONUP:
+                gun.bulletShot(gun.bulletLeft)
                 gun.update(True)
             if event.type == pygame.KEYUP and event.key == pygame.K_r:
-                # idk what to do but keep it this the second 6 is only needed value ie bullet
-                gun.update(6, 7)
+                if gun.bulletLeft < 6:
+                    bulletReloadedAmount = 6
+                    if gun.gunReload():
+                        gun.update(6, bulletReloadedAmount)
 
         gun.update()
         pygame.display.flip()
         clock.tick(24)
-
 
 def loadImages(folderPath, imageSize):
     image_files = [imageFile for imageFile in os.listdir(folderPath) if imageFile.lower().endswith('.png')]
@@ -124,7 +145,6 @@ def loadImages(folderPath, imageSize):
         image = pygame.transform.scale(image, imageSize)
         images.append(image)
     return images
-
 
 if __name__ == "__main__":
     main()
